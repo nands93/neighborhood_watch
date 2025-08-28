a2344326c7a04d093889c841ec7476cd4806f9f6
nands93 <femmarques93+github@gmail.com>
2025-08-28 15:44:15 -0300

    Add user and alert models with necessary fields and JSON tags

---
+++ backend/internal/model/alerts.go
@@ -0,0 +1,19 @@
+package model
+
+import "time"
+
+type Point struct {
+	Lat  float64 `json:"latitude"`
+	Long float64 `json:"longitude"`
+}
+
+type Alert struct {
+	ID          int64     `json:"id" db:"id"`
+	Title       string    `json:"title" db:"title"`
+	Description string    `json:"description" db:"description"`
+	Category    string    `json:"category" db:"category"`
+	Location    Point     `json:"location" db:"location"`
+	UserID      int64     `json:"user_id" db:"user_id"`
+	CreatedAt   time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at" db:"updated_at"`
+}
